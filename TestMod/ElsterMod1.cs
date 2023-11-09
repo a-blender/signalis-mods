b05f0b151b00bb4d10533b7d5bb08333382e60f9
Andy Blendermann <andyblendermann@gmail.com>
2023-11-09 12:46:56 -0700

    Add signalis test mod files and gitignore

---
+++ TestMod/ElsterMod1.cs
@@ -0,0 +1,23 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ...
+[assembly: MelonInfo(typeof(ElsterMod1), "My first Elster mod", "latest", "Andy Blendermann")]
+[assembly: MelonGame("", "")]
+
+namespace TestMod
+{
+    // todo: create a namespace for mods and a namespace for each mod. Add using <namespace> to the
+    // imports list.
+
+    // todo: rename class to the function of your mod (Example: 'AddSword', 'CombatMode')
+
+    public class ElsterMod1 : MelonMod
+    {
+        // todo: add code!
+    }
+}
